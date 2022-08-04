069fcbf41998d098af028b98a8d53a53988d948d
Aryan <aryanrsuri@gmail.com>
2022-08-03 19:03:58 -0700

    1.0.0

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ let game = {
   guesses: [],
   eval: [],
   status: "in_progress",
+  solution: "manor",
 };
 
 localStorage.setItem("wordle", game);
@@ -64,6 +65,9 @@ async function render(keypress) {
 }
 
 async function evaluate() {
+  if (game.pos[1] > 30) {
+    return lose();
+  }
   const client = await fetch(URL)
   .then((response) => {
     if (response.status !== 200) {
@@ -79,6 +83,24 @@ async function evaluate() {
     throw "Word already used!"
   }
   if (client.includes(game.current)) {
+    if (game.current == game.solution) {
+      document.getElementById("out").innerHTML = `You won!`;
+      return win()
+    }
+
+    for (let i in game.current) {
+      let no = parseInt(i) + 1; 
+      if (game.solution.includes(game.current[i])) {
+        curr_pos = 5*game.pos[0] +no;
+        if (game.solution[i] == game.current[i]) {
+          addClasstoBox(curr_pos, "correct");
+        }
+        addClasstoBox(curr_pos, "used")
+      } else {
+        addClasstoBox(no, "absent")
+      }
+    }
+
     game.guesses.push(game.current);
     game.pos[0] += 1;
 
@@ -89,7 +111,7 @@ async function evaluate() {
       document.getElementById(i).innerHTML = ``;
     }
     game.current = "";
-    game.pos[1] = 1;
+    game.pos[1] = 5*game.pos[0] + 1;
     del();
     throw "Not a real word";
   }
@@ -99,7 +121,38 @@ async function del() {
   //deletes char on backspace
 }
 
+async function win() {
+  game.status = "won";
+  await addClasstoLine("correct")
+}
 
-async function clearLine() {
+async function lose() {
+  game.status = "lost";
+  clearGrid();
+  document.getElementById("out").innerHTML = `You lost :(, the answer was ${game.solution}`; 
+}
 
+
+async function addClasstoLine(_class) {
+  for (let i=5*game.pos[0] + 1; i < game.pos[1]; i++) {
+    Promise.resolve(addClasstoBox(i, _class));
+  } 
+}
+async function addClasstoBox(i, _class) {
+  let box = document.getElementById(i);
+  box.classList.add(_class);
+}
+
+function clearLine(i) {
+  // for (let i=5*game.pos[0] + 1; i <game.pos[1]; i++) {
+  //   document.getElementById(i).innerHTML = ``;
+  // }
+}
+
+function clearGrid() {
+  for (let i=1; i < 31; i++) {
+    let box = document.getElementById(i);
+    box.classList.remove("absent", "used")
+    box.innerHTML = ``;
+  }
 }
