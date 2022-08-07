a14d6af8ba9234db57b612745d4f90585c78e2b3
Aryan <aryanrsuri@gmail.com>
2022-08-07 14:16:50 -0700

    using typescript

--- src/index.js
+++ src/index.js
@@ -1,185 +1,307 @@
-const GRID = document.querySelector("div.grid");
-const KEYBOARD = document.querySelector("div.keygrid");
-const URL =
-  "https://raw.githubusercontent.com/tabatkins/wordle-list/main/words";
-const client = async () => {
-  try {
-    const response = await fetch(URL);
-    if (response.status !== 200) {
-      throw response.status;
-    }
-    return await response.text();
-  } catch (Error) {
-    throw Error;
-  }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-
-let game = {
-  current: "",
-  pos: [0, 1],
-  guesses: [],
-  eval: [],
-  status: "in_progress",
-  solution: "",
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
 };
-
-async function init() {
-  try {
-    const words = await client();
-    game.solution =
-      words.split("\n")[Math.floor(Math.random() * words.split("\n").length)];
-
-    for (let i = 1; i < 31; i++) {
-      let box = document.createElement("div");
-      box.classList.add("box");
-      box.id = i;
-      GRID.appendChild(box);
-    }
-
-    for (let i = 0; i < 26; i++) {
-      let char = document.createElement("div");
-      char.classList.add("box");
-      char.id = String.fromCharCode(65 + i);
-      char.innerHTML = char.id;
-      KEYBOARD.appendChild(char);
-    }
-  } catch (Error) {
-    throw Error;
-  }
-}
-
-document.addEventListener("keydown", async (key) => {
-  await render(key);
-});
-
-async function render(keypress) {
-  if (keypress.key === "Backspace") {
-    try {
-      if (game.pos[1] > 5 * game.pos[0] + 1) {
-        document.getElementById(game.pos[1] - 1).innerHTML = ``;
-        game.pos[1] < 1 ? (game.pos[1] = 1) : (game.pos[1] -= 1);
-        game.current = game.current.slice(0, -1);
-        return;
-      }
-
-      return;
-    } catch (Error) {
-      throw Error;
-    }
-  }
-  if (keypress.key === "Enter") {
-    if (game.current.length == 5) {
-      return evaluate();
-    }
-    throw "Length not 5!";
-  }
-  if (keypress.keyCode > 64 && keypress.keyCode < 91) {
-    if (game.current.length < 5) {
-      try {
-        game.current += keypress.key;
-        document.getElementById(game.pos[1]).innerHTML = game.current.charAt(
-          game.current.length - 1
-        );
-        game.pos[1] > 30 ? (game.pos[1] = 30) : (game.pos[1] += 1);
-
-        return;
-      } catch (Error) {
-        return Promise.reject(Error);
-      }
-    } // [TODO] UX.1 Add a row feedback, max letters alloted 
-  } else {
-    throw "Not a letter!";
-  }
-}
-
-async function evaluate() {
-  await clearOut();
-  let wordlist = await client();
-  
-  if (wordlist.includes(game.current)) {
-    let row = [];
-    for (let i in game.current) {
-      const no = parseInt(i) + 1;
-      const curr_pos = 5 * game.pos[0] + no;
-      const charID = game.current[i].toUpperCase();
-      if (game.solution.includes(game.current[i])) {
-        if (game.solution[i] == game.current[i]) {
-          row.push("correct");
-          addClasstoBox(curr_pos, "correct");
-          addClasstoBox(charID, "correct");
+var _this = this;
+var GRID = document.querySelector("div.grid");
+var KEYBOARD = document.querySelector("div.keygrid");
+var uri = "https://raw.githubusercontent.com/tabatkins/wordle-list/main/words";
+var client = function () { return __awaiter(_this, void 0, void 0, function () {
+    var response, Error_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 3, , 4]);
+                return [4 /*yield*/, fetch(uri)];
+            case 1:
+                response = _a.sent();
+                if (response.status !== 200) {
+                    throw response.status;
+                }
+                return [4 /*yield*/, response.text()];
+            case 2: return [2 /*return*/, _a.sent()];
+            case 3:
+                Error_1 = _a.sent();
+                throw Error_1;
+            case 4: return [2 /*return*/];
         }
-        row.push("used")
-        addClasstoBox(curr_pos, "used");
-        addClasstoBox(charID, "used");
-      } else {
-        row.push("absent")
-        addClasstoBox(curr_pos, "absent");
-        addClasstoBox(charID, "absent");
-      }
-    }
-    
-    game.guesses.push(game.current);
-    game.eval.push(row);
-    game.pos[0] += 1;
-    game.current = "";
-
-    if (game.current == game.solution) {
-      await writeOut(`You won! the word was ${game.current}`);
-      return win();
-    }
-    if (game.pos[1] > 30) {
-      return lose();
-    }
-    return
-  } else {
-    for (let i = game.pos[1] - 5; i < game.pos[1]; i++) {
-      document.getElementById(i).innerHTML = ``;
-    }
-    await writeOut(`${game.current} is not a word!`);
-    game.current = "";
-    game.pos[1] = 5 * game.pos[0] + 1;
-    throw "Not a real word";
-  }
+    });
+}); };
+var game = {
+    current: "",
+    pos: [0, 1],
+    guesses: [],
+    eval: [],
+    status: "in_progress",
+    solution: ""
+};
+function init() {
+    return __awaiter(this, void 0, void 0, function () {
+        var words, i, box, i, char, Error_2;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 2, , 3]);
+                    return [4 /*yield*/, client()];
+                case 1:
+                    words = _a.sent();
+                    game.solution =
+                        words.split("\n")[Math.floor(Math.random() * words.split("\n").length)];
+                    for (i = 1; i < 31; i++) {
+                        box = document.createElement("div");
+                        box.classList.add("box");
+                        box.id = "".concat(i);
+                        GRID.appendChild(box);
+                    }
+                    for (i = 0; i < 26; i++) {
+                        char = document.createElement("div");
+                        char.classList.add("box");
+                        char.id = String.fromCharCode(65 + i);
+                        char.innerHTML = char.id;
+                        KEYBOARD.appendChild(char);
+                    }
+                    return [3 /*break*/, 3];
+                case 2:
+                    Error_2 = _a.sent();
+                    throw Error_2;
+                case 3: return [2 /*return*/];
+            }
+        });
+    });
 }
-
-async function win() {
-  game.status = "won";
-  await addClasstoLine("correct");
+document.addEventListener("keydown", function (key) { return __awaiter(_this, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, render(key)];
+            case 1:
+                _a.sent();
+                return [2 /*return*/];
+        }
+    });
+}); });
+function render(keypress) {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            if (keypress.key === "Backspace") {
+                try {
+                    if (game.pos[1] > 5 * game.pos[0] + 1) {
+                        document.getElementById("".concat(game.pos[1] - 1)).innerHTML = "";
+                        game.pos[1] < 1 ? (game.pos[1] = 1) : (game.pos[1] -= 1);
+                        game.current = game.current.slice(0, -1);
+                        return [2 /*return*/];
+                    }
+                    return [2 /*return*/];
+                }
+                catch (Error) {
+                    throw Error;
+                }
+            }
+            if (keypress.key === "Enter") {
+                if (game.current.length == 5) {
+                    return [2 /*return*/, evaluate()];
+                }
+                throw "Length not 5!";
+            }
+            if (keypress.keyCode > 64 && keypress.keyCode < 91) {
+                if (game.current.length < 5) {
+                    try {
+                        game.current += keypress.key;
+                        document.getElementById("".concat(game.pos[1])).innerHTML = game.current.charAt(game.current.length - 1);
+                        game.pos[1] > 30 ? (game.pos[1] = 30) : (game.pos[1] += 1);
+                        return [2 /*return*/];
+                    }
+                    catch (Error) {
+                        return [2 /*return*/, Promise.reject(Error)];
+                    }
+                } // [TODO] UX.1 Add a row feedback, max letters alloted 
+            }
+            else {
+                throw "Not a letter!";
+            }
+            return [2 /*return*/];
+        });
+    });
 }
-
-async function lose() {
-  game.status = "lost";
-  clearGrid();
-  await writeOut(`You lost! the word was ${game.solution}`);
+function evaluate() {
+    return __awaiter(this, void 0, void 0, function () {
+        var wordlist, row, i, no, curr_pos, charID, i;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, clearOut()];
+                case 1:
+                    _a.sent();
+                    return [4 /*yield*/, client()];
+                case 2:
+                    wordlist = _a.sent();
+                    if (!wordlist.includes(game.current)) return [3 /*break*/, 5];
+                    row = [];
+                    for (i in game.current) {
+                        no = parseInt(i) + 1;
+                        curr_pos = 5 * game.pos[0] + no;
+                        charID = game.current[i].toUpperCase();
+                        if (game.solution.includes(game.current[i])) {
+                            if (game.solution[i] == game.current[i]) {
+                                row.push("correct");
+                                addClasstoBox(curr_pos, "correct");
+                                addClasstoBox(charID, "correct");
+                            }
+                            row.push("used");
+                            addClasstoBox(curr_pos, "used");
+                            addClasstoBox(charID, "used");
+                        }
+                        else {
+                            row.push("absent");
+                            addClasstoBox(curr_pos, "absent");
+                            addClasstoBox(charID, "absent");
+                        }
+                    }
+                    game.guesses.push(game.current);
+                    game.eval.push(row);
+                    game.pos[0] += 1;
+                    game.current = "";
+                    if (!(game.current == game.solution)) return [3 /*break*/, 4];
+                    return [4 /*yield*/, writeOut("You won! the word was ".concat(game.current))];
+                case 3:
+                    _a.sent();
+                    return [2 /*return*/, win()];
+                case 4:
+                    if (game.pos[1] > 30) {
+                        return [2 /*return*/, lose()];
+                    }
+                    return [2 /*return*/];
+                case 5:
+                    for (i = game.pos[1] - 5; i < game.pos[1]; i++) {
+                        document.getElementById("".concat(i)).innerHTML = "";
+                    }
+                    return [4 /*yield*/, writeOut("".concat(game.current, " is not a word!"))];
+                case 6:
+                    _a.sent();
+                    game.current = "";
+                    game.pos[1] = 5 * game.pos[0] + 1;
+                    throw "Not a real word";
+            }
+        });
+    });
 }
-
-async function addClasstoLine(_class) {
-  for (let i = 5 * game.pos[0] + 1; i < game.pos[1]; i++) {
-    Promise.resolve(addClasstoBox(i, _class));
-  }
+function win() {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    game.status = "won";
+                    return [4 /*yield*/, addClasstoLine("correct")];
+                case 1:
+                    _a.sent();
+                    return [2 /*return*/];
+            }
+        });
+    });
 }
-async function addClasstoBox(i, _class) {
-  let box = document.getElementById(i);
-  box.classList.add(_class);
+function lose() {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    game.status = "lost";
+                    clearGrid();
+                    return [4 /*yield*/, writeOut("You lost! the word was ".concat(game.solution))];
+                case 1:
+                    _a.sent();
+                    return [2 /*return*/];
+            }
+        });
+    });
 }
-
-async function addClasstoChar(charid, _class) {
-  let char = document.getElementById(charid);
-  char.classList.add(_class);
+function addClasstoLine(_class) {
+    return __awaiter(this, void 0, void 0, function () {
+        var i;
+        return __generator(this, function (_a) {
+            for (i = 5 * game.pos[0] + 1; i < game.pos[1]; i++) {
+                Promise.resolve(addClasstoBox(i, _class));
+            }
+            return [2 /*return*/];
+        });
+    });
 }
-
-async function clearGrid() {
-  for (let i = 1; i < 31; i++) {
-    let box = document.getElementById(i);
-    //box.classList.remove("absent", "used");
-    box.innerHTML = ``;
-  }
+function addClasstoBox(i, _class) {
+    return __awaiter(this, void 0, void 0, function () {
+        var box;
+        return __generator(this, function (_a) {
+            box = document.getElementById("".concat(i));
+            box === null || box === void 0 ? void 0 : box.classList.add(_class);
+            return [2 /*return*/];
+        });
+    });
 }
-async function writeOut(text) {
-  document.getElementById("out").innerHTML = text;
+function addClasstoChar(i, _class) {
+    return __awaiter(this, void 0, void 0, function () {
+        var char;
+        return __generator(this, function (_a) {
+            char = document.getElementById("".concat(i));
+            char === null || char === void 0 ? void 0 : char.classList.add(_class);
+            return [2 /*return*/];
+        });
+    });
 }
-async function clearOut() {
-  document.getElementById("out").innerHTML = ``;
+function clearGrid() {
+    return __awaiter(this, void 0, void 0, function () {
+        var i, box;
+        return __generator(this, function (_a) {
+            for (i = 1; i < 31; i++) {
+                box = document.getElementById("".concat(i));
+                //box.classList.remove("absent", "used");
+                box.innerHTML = "";
+            }
+            return [2 /*return*/];
+        });
+    });
 }
-
-document.body.addEventListener("load", init());
+function writeOut(text) {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            document.getElementById("out").innerHTML = text;
+            return [2 /*return*/];
+        });
+    });
+}
+function clearOut() {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            document.getElementById("out").innerHTML = "";
+            return [2 /*return*/];
+        });
+    });
+}
+init();
